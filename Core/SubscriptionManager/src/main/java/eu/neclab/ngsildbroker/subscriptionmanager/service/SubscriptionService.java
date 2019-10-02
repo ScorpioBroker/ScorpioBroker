16c898cc6e0d189d26507745fe216e6ac60359f7
benni <hebgen@neclab.eu>
2019-10-02 11:32:33 +0200

    fixed limit bug

--- Core/SubscriptionManager/src/main/java/eu/neclab/ngsildbroker/subscriptionmanager/service/SubscriptionService.java
+++ Core/SubscriptionManager/src/main/java/eu/neclab/ngsildbroker/subscriptionmanager/service/SubscriptionService.java
@@ -344,7 +344,7 @@ public class SubscriptionService implements SubscriptionManager {
 		List<Subscription> result = new ArrayList<Subscription>();
 		result.addAll(subscriptionId2Subscription.values());
 		if (limit > 0) {
-			if (limit > result.size()) {
+			if (limit < result.size()) {
 				result = result.subList(0, limit);
 			}
 		}
