b0d1359d2ee2a69967d809825c6aa631de8a6d97
Benjamin Hebgen <scorpiobroker@neclab.eu>
2019-10-14 08:44:20 +0000

    added core css

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ pygments_style = None
 
 def setup(app):
     app.add_stylesheet('css/fiware_readthedocs.css')
-    app.add_stylesheet('css/fiware_readthedocs_processing.css')
+    app.add_stylesheet('css/fiware_readthedocs_core.css')
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
