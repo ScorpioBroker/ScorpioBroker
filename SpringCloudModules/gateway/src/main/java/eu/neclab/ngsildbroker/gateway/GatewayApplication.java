cdbdc820aa9cc788c4b191eeeecf187c337628e2
benni <hebgen@neclab.eu>
2020-01-15 11:03:48 +0100

    added some special characters to allowed characters addressed Issue #58

--- SpringCloudModules/gateway/src/main/java/eu/neclab/ngsildbroker/gateway/GatewayApplication.java
+++ SpringCloudModules/gateway/src/main/java/eu/neclab/ngsildbroker/gateway/GatewayApplication.java
@@ -1,8 +1,12 @@
 package eu.neclab.ngsildbroker.gateway;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -88,5 +92,16 @@ public class GatewayApplication {
 		
 		
 		return new CorsFilter(source);
+	}
+	@Bean
+	public ConfigurableServletWebServerFactory webServerFactory() {
+	    TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+	    factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+	        @Override
+	        public void customize(Connector connector) {
+	            connector.setProperty("relaxedQueryChars", "|{}[]");
+	        }
+	    });
+	    return factory;
 	}
 }
