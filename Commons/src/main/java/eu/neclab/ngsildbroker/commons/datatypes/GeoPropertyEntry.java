f72a2379295b0314481aa648deab5ecc7b352c2d
Benjamin Hebgen <hebgen@office.hd>
2020-04-22 10:45:11 +0200

    fixed null pointer in deserialization

--- Commons/src/main/java/eu/neclab/ngsildbroker/commons/datatypes/GeoPropertyEntry.java
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/datatypes/GeoPropertyEntry.java
@@ -2,11 +2,14 @@ package eu.neclab.ngsildbroker.commons.datatypes;
 
 import com.github.filosganga.geogson.model.Geometry;
 
+import eu.neclab.ngsildbroker.commons.constants.NGSIConstants;
+
 public class GeoPropertyEntry extends BaseEntry {
 	public GeoPropertyEntry(String dataSetId, String value, Geometry<?> geoValue) {
 		super(dataSetId);
 		this.value = value;
 		this.geoValue = geoValue;
+		this.type = NGSIConstants.NGSI_LD_GEOPROPERTY;
 	}
 
 	private String value;
