04e2b609654b8d3b3086ca18b7a06c3f5b1bc37e
ScorpioBroker <55130839+ScorpioBroker@users.noreply.github.com>
2020-08-30 21:29:37 +0200

    added health and corecontext endpoint

--- Core/InfoServer/src/main/java/eu/neclab/ngsildbroker/infoserver/InfoServer.java
+++ Core/InfoServer/src/main/java/eu/neclab/ngsildbroker/infoserver/InfoServer.java
@@ -2,27 +2,21 @@ package eu.neclab.ngsildbroker.infoserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.client.RestTemplate;
 
-import eu.neclab.ngsildbroker.commons.ldcontext.AtContext;
-import eu.neclab.ngsildbroker.commons.ldcontext.AtContextProducerChannel;
-import eu.neclab.ngsildbroker.commons.ldcontext.ContextResolverBasic;
 import eu.neclab.ngsildbroker.commons.securityConfig.ResourceConfigDetails;
 import eu.neclab.ngsildbroker.commons.securityConfig.SecurityConfig;
 import eu.neclab.ngsildbroker.commons.stream.service.KafkaConfig;
-import eu.neclab.ngsildbroker.commons.stream.service.KafkaOps;
 import eu.neclab.ngsildbroker.commons.swaggerConfig.SwaggerConfigDetails;
 
 
 @SpringBootApplication
 @Import({KafkaConfig.class, SwaggerConfigDetails.class})
-public class InfoContextServer {// implements QueryHandlerInterface{
+public class InfoServer {// implements QueryHandlerInterface{
 
 	public static void main(String[] args) {
-		SpringApplication.run(InfoContextServer.class, args);
+		SpringApplication.run(InfoServer.class, args);
 	}
 
 	
