7007be88532596652363a50eaacd0f9a80e02c77
hiteshNEC <69617762+hiteshNEC@users.noreply.github.com>
2020-09-22 19:47:42 +0530

    UT test cases Subscription Manager (#129)
    * UT test cases Subscription Manager
    
    * reviews and comments has been incorporated
    
    * minor change in subscription ID
    
    * Update SubscriptionControllerTest.java
    
    fixed indentation

---
+++ Core/SubscriptionManager/src/test/java/eu/neclab/ngsildbroker/subscriptionmanager/SubscriptionHandlerTest.java
@@ -0,0 +1,13 @@
+package eu.neclab.ngsildbroker.subscriptionmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties= {"spring.main.allow-bean-definition-overriding=true"})
+public class SubscriptionHandlerTest {
+	@Test
+	public void contextLoads() {
+	}
+}
