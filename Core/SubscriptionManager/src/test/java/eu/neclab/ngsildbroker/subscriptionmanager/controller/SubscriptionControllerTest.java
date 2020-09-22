86fdfb95ed99e5f099e5fd707c60f4a7143f86bb
ScorpioBroker <55130839+ScorpioBroker@users.noreply.github.com>
2020-09-22 11:00:55 +0200

    Update SubscriptionControllerTest.java
    fixed indentation

--- Core/SubscriptionManager/src/test/java/eu/neclab/ngsildbroker/subscriptionmanager/controller/SubscriptionControllerTest.java
+++ Core/SubscriptionManager/src/test/java/eu/neclab/ngsildbroker/subscriptionmanager/controller/SubscriptionControllerTest.java
@@ -43,26 +43,26 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @AutoConfigureMockMvc//(secure = false)
 public class SubscriptionControllerTest {
 	@Autowired
-    private MockMvc mockMvc;
+	private MockMvc mockMvc;
 	
 	@MockBean
-    private SubscriptionService subscriptionService;
-    
-    
-    @InjectMocks
-    private SubscriptionController subscriptionController;
-    
-    
-    @Autowired
-    ContextResolverBasic contextResolver;
+	private SubscriptionService subscriptionService;
+	
+	
+	@InjectMocks
+	private SubscriptionController subscriptionController;
+	
+	
+	@Autowired
+	ContextResolverBasic contextResolver;
    
 	@Autowired
 	ParamsResolver paramsResolver;
 
-    @Mock
-    SubscriptionManagerProducerChannel producerChannel;
-    
-    private String subscriptionEntityPayload;
+	@Mock
+	SubscriptionManagerProducerChannel producerChannel;
+	
+	private String subscriptionEntityPayload;
 	
 
 	@Before	
@@ -75,21 +75,21 @@ public class SubscriptionControllerTest {
 				+ "\r\n\"id\": \"urn:ngsi-ld:Subscription:211\","
 				+ "\r\n\"type\": \"Subscription\","
 				+ "\r\n\"entities\": [{"
-				+ "\r\n          \"id\": \"urn:ngsi-ld:Vehicle:A143\","
-				+ "\r\n          \"type\": \"Vehicle\""
-				+ "\r\n        }],"
+				+ "\r\n		  \"id\": \"urn:ngsi-ld:Vehicle:A143\","
+				+ "\r\n		  \"type\": \"Vehicle\""
+				+ "\r\n		}],"
 				+ "\r\n\"watchedAttributes\": [\"brandName\"],"
-				+ "\r\n        \"q\":\"brandName!=Mercedes\","
+				+ "\r\n		\"q\":\"brandName!=Mercedes\","
 				+ "\r\n\"notification\": {"
 				+ "\r\n  \"attributes\": [\"brandName\"],"
 				+ "\r\n  \"format\": \"keyValues\","
 				+ "\r\n  \"endpoint\": {"
 				+ "\r\n   \"uri\": \"mqtt://localhost:1883/notify\","
 				+ "\r\n   \"accept\": \"application/json\","
-				+ "\r\n    \"notifierinfo\": {"
-				+ "\r\n      \"version\" : \"mqtt5.0\","
-				+ "\r\n      \"qos\" : 0"
-				+ "\r\n    }"
+				+ "\r\n	\"notifierinfo\": {"
+				+ "\r\n	  \"version\" : \"mqtt5.0\","
+				+ "\r\n	  \"qos\" : 0"
+				+ "\r\n	}"
 				+ "\r\n  }"
 				+ "\r\n}"
 				+ "\r\n}";
@@ -103,7 +103,7 @@ public class SubscriptionControllerTest {
 		subscriptionEntityPayload=null;
 		
 	}
-    
+	
 	/**
 	 * this method is use for subscribe the entity
 	 */
@@ -167,14 +167,14 @@ public class SubscriptionControllerTest {
 	public void getSubscriptionEntityTest() {
 		try {
 			List<Object> context = new ArrayList<>();	
-		    Subscription subscription = null;
-		    subscription = DataSerializer.getSubscription(subscriptionEntityPayload);
-		    when(subscriptionService.getSubscription(any())).thenReturn(subscription);
+			Subscription subscription = null;
+			subscription = DataSerializer.getSubscription(subscriptionEntityPayload);
+			when(subscriptionService.getSubscription(any())).thenReturn(subscription);
 			mockMvc.perform(get("/ngsi-ld/v1/subscriptions/urn:ngsi-ld:Subscription:211")
 					.accept(AppConstants.NGB_APPLICATION_JSON)).andExpect(status().isOk());
 			verify(subscriptionService, times(1)).getSubscription(any());
-           } catch (Exception e) {
-        	   e.printStackTrace();
+		   } catch (Exception e) {
+			   e.printStackTrace();
 		}
 	}
 	
