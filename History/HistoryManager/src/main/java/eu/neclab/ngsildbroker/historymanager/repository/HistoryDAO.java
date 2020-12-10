500d0ebfb6dee13fb49a21a244ccdb20ccf0488b
Amit Kumar Raghav <Amit.Raghav@nectechnologies.in>
2020-12-10 14:08:17 +0530

    Changes in HistoryDAO class for check default ObseredAt.

--- History/HistoryManager/src/main/java/eu/neclab/ngsildbroker/historymanager/repository/HistoryDAO.java
+++ History/HistoryManager/src/main/java/eu/neclab/ngsildbroker/historymanager/repository/HistoryDAO.java
@@ -131,10 +131,21 @@ public class HistoryDAO extends StorageReaderDAO {
 		}
 		sqlQuery += "  as tedata, " + "jsonb_object_agg(attributeid,";
 		if (qp.getTemporalValues()) {
-			sqlQuery += "(select json_agg(jsonb_build_array(t -> '"+NGSIConstants.NGSI_LD_HAS_VALUE+"'->0->'"+NGSIConstants.JSON_LD_VALUE +"',t->'"+NGSIConstants.NGSI_LD_OBSERVED_AT+"'->0->'"+NGSIConstants.JSON_LD_VALUE+"')) from jsonb_array_elements(attributedata) as x(t))";
-		}
-		else {
-			sqlQuery +="attributedata";
+			if (qp.getTimeproperty().equalsIgnoreCase(NGSIConstants.NGSI_LD_MODIFIED_AT)) {
+				sqlQuery += "(select json_agg(jsonb_build_array(t -> '" + NGSIConstants.NGSI_LD_HAS_VALUE + "'->0->'"
+						+ NGSIConstants.JSON_LD_VALUE + "',t->'" + NGSIConstants.NGSI_LD_MODIFIED_AT + "'->0->'"
+						+ NGSIConstants.JSON_LD_VALUE + "')) from jsonb_array_elements(attributedata) as x(t))";
+			} else if (qp.getTimeproperty().equalsIgnoreCase(NGSIConstants.NGSI_LD_CREATED_AT)) {
+				sqlQuery += "(select json_agg(jsonb_build_array(t -> '" + NGSIConstants.NGSI_LD_HAS_VALUE + "'->0->'"
+						+ NGSIConstants.JSON_LD_VALUE + "',t->'" + NGSIConstants.NGSI_LD_CREATED_AT + "'->0->'"
+						+ NGSIConstants.JSON_LD_VALUE + "')) from jsonb_array_elements(attributedata) as x(t))";
+			} else if (qp.getTimeproperty().equalsIgnoreCase(NGSIConstants.NGSI_LD_OBSERVED_AT)) {
+				sqlQuery += "(select json_agg(jsonb_build_array(t -> '" + NGSIConstants.NGSI_LD_HAS_VALUE + "'->0->'"
+						+ NGSIConstants.JSON_LD_VALUE + "',t->'" + NGSIConstants.NGSI_LD_OBSERVED_AT + "'->0->'"
+						+ NGSIConstants.JSON_LD_VALUE + "')) from jsonb_array_elements(attributedata) as x(t))";
+			}
+		} else {
+			sqlQuery += "attributedata";
 		}
 
 		sqlQuery += ") as attrdata " + "  from r ";
