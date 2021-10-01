c878df024483b0062e5843b66dde97393786215d
Benjamin Hebgen <benjamin.hebgen@neclab.eu>
2021-10-01 12:09:29 +0200

    fixed type check

--- Core/QueryManager/src/main/java/eu/neclab/ngsildbroker/queryhandler/controller/EntityOperationQueryController.java
+++ Core/QueryManager/src/main/java/eu/neclab/ngsildbroker/queryhandler/controller/EntityOperationQueryController.java
@@ -151,11 +151,13 @@ public class EntityOperationQueryController {
 					params.setQ(queryParser.parseQuery((String) getValue(entry.getValue()), linkHeaders).toSql(false));
 					break;
 				case NGSIConstants.JSON_LD_TYPE:
-					if (!entry.getValue().toString().equals(NGSIConstants.QUERY_TYPE)) {
-						throw new ResponseException(ErrorType.BadRequestData,
-								"Type has to be Query for this operation");
+					if (entry.getValue() instanceof List) {
+						if (((List) entry.getValue()).get(0).toString()
+								.equals(NGSIConstants.NGSI_LD_DEFAULT_PREFIX + NGSIConstants.QUERY_TYPE)) {
+							break;
+						}
 					}
-					break;
+					throw new ResponseException(ErrorType.BadRequestData, "Type has to be Query for this operation");
 
 				default:
 					throw new ResponseException(ErrorType.BadRequestData, entry.getKey() + " is an unknown entry");
