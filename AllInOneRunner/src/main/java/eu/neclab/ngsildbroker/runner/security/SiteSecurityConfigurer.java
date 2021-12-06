02cb905b8e237306e6386161ff0cdfc282c898cb
ScorpioBroker <55130839+ScorpioBroker@users.noreply.github.com>
2021-12-06 11:26:04 +0100

    Update SiteSecurityConfigurer.java

--- AllInOneRunner/src/main/java/eu/neclab/ngsildbroker/runner/security/SiteSecurityConfigurer.java
+++ AllInOneRunner/src/main/java/eu/neclab/ngsildbroker/runner/security/SiteSecurityConfigurer.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableOAuth2Sso
+//@EnableOAuth2Sso
 @EnableWebSecurity
 public class SiteSecurityConfigurer extends WebSecurityConfigurerAdapter {
 	
