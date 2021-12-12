48a12eda07016656c7017deea3b59377b9a61c6f
Benjamin Hebgen <benjamin.hebgen@neclab.eu>
2021-12-12 22:44:04 +0100

    one more try with more nativehints

--- AllInOneRunner/src/main/java/eu/neclab/ngsildbroker/runner/Runner.java
+++ AllInOneRunner/src/main/java/eu/neclab/ngsildbroker/runner/Runner.java
@@ -9,17 +9,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
 import eu.neclab.ngsildbroker.entityhandler.EntityHandler;
+import eu.neclab.ngsildbroker.entityhandler.controller.EntityController;
 import eu.neclab.ngsildbroker.historymanager.HistoryHandler;
 import eu.neclab.ngsildbroker.queryhandler.QueryHandler;
 import eu.neclab.ngsildbroker.registryhandler.RegistryHandler;
 import eu.neclab.ngsildbroker.subscriptionmanager.SubscriptionHandler;
 
 @SpringBootApplication
-@NativeHint(options = {"--enable-all-security-services"},resources = {
+@NativeHint(options = { "--enable-all-security-services" }, resources = {
 		@ResourceHint(patterns = "eu/neclab/ngsildbroker/*/controller/*.java"),
 		@ResourceHint(patterns = "org/flywaydb/core/internal/version.txt") }, types = {
-				@TypeHint(types = { RestController.class, Service.class }) })
-
+				@TypeHint(types = { EntityController.class }) })
 public class Runner {
 
 	public static void main(String[] args) throws Exception {
