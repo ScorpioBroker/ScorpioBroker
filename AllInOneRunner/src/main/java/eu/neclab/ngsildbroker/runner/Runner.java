e55a6c9ddcafcf1b009a4cef29c497f11bb2e790
Benjamin Hebgen <benjamin.hebgen@neclab.eu>
2021-12-12 22:31:17 +0100

    one more try with more nativehints

--- AllInOneRunner/src/main/java/eu/neclab/ngsildbroker/runner/Runner.java
+++ AllInOneRunner/src/main/java/eu/neclab/ngsildbroker/runner/Runner.java
@@ -2,7 +2,11 @@ package eu.neclab.ngsildbroker.runner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.ResourceHint;
+import org.springframework.nativex.hint.TypeHint;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
 import eu.neclab.ngsildbroker.entityhandler.EntityHandler;
 import eu.neclab.ngsildbroker.historymanager.HistoryHandler;
@@ -11,8 +15,11 @@ import eu.neclab.ngsildbroker.registryhandler.RegistryHandler;
 import eu.neclab.ngsildbroker.subscriptionmanager.SubscriptionHandler;
 
 @SpringBootApplication
-@ResourceHint(patterns = "eu/neclab/ngsildbroker.entityhandler.controller.EntityController.java")
-@ResourceHint(patterns = "org/flywaydb/core/internal/version.txt")
+@NativeHint(options = { " — enable-all-security-services" }, resources = {
+		@ResourceHint(patterns = "eu/neclab/ngsildbroker/*/controller/*.java"),
+		@ResourceHint(patterns = "org/flywaydb/core/internal/version.txt") }, types = {
+				@TypeHint(types = { RestController.class, Service.class }) })
+
 public class Runner {
 
 	public static void main(String[] args) throws Exception {
