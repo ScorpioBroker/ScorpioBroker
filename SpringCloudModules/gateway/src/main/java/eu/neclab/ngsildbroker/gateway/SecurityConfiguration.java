0261b60fce9dcc057e3d3cdd0084f89e9bd0dab5
Benjamin Hebgen <benjamin.hebgen@neclab.eu>
2021-12-13 16:54:21 +0100

    fixing defaults for security

---
+++ SpringCloudModules/gateway/src/main/java/eu/neclab/ngsildbroker/gateway/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+
+package eu.neclab.ngsildbroker.gateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+	@Value("${spring.security.active:false}")
+	boolean securityActive;
+
+	@Bean
+	public SecurityWebFilterChain configure(ServerHttpSecurity http, Environment env) {
+		http.cors();
+		if (securityActive) { // http.oauth2ResourceServer().jwt();
+			http.authorizeExchange().anyExchange().authenticated().and().httpBasic().and().oauth2Client().and()
+					.oauth2Login().and().formLogin();
+		} else {
+			http.authorizeExchange().anyExchange().permitAll();
+		}
+		return http.build();
+	}
+}
