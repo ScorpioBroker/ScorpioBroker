6a1e045721faaef09523ee6db248752d071235cb
Benjamin Hebgen <benjamin.hebgen@neclab.eu>
2021-12-15 17:14:54 +0100

    fixed atcontext handling

--- Core/QueryManager/src/main/java/eu/neclab/ngsildbroker/queryhandler/QueryHandler.java
+++ Core/QueryManager/src/main/java/eu/neclab/ngsildbroker/queryhandler/QueryHandler.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.web.client.RestTemplate;
 
