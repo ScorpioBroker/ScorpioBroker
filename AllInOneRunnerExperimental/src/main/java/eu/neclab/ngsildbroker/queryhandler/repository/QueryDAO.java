56321b09edf2fb6227da02453258e1e3c977e370
Benjamin Hebgen <benjamin.hebgen@neclab.eu>
2022-01-13 10:53:51 +0100

    removed experimental for now move to graalvm after the release

--- AllInOneRunnerExperimental/src/main/java/eu/neclab/ngsildbroker/queryhandler/repository/QueryDAO.java
+++
@@ -1,9 +0,0 @@
-package eu.neclab.ngsildbroker.queryhandler.repository;
-
-import org.springframework.stereotype.Repository;
-import eu.neclab.ngsildbroker.commons.storage.StorageReaderDAO;
-
-@Repository
-public class QueryDAO extends StorageReaderDAO {
-	
-}
