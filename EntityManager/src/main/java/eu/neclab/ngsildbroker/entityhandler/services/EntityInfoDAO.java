d06551fc5e32c3b2811785d22f51280aaf5d0a8f
Pawan Kumar <Pawan.Kumar3@nectechnologies.in>
2022-05-23 12:25:46 +0530

    Resolve issue in entitymanager now entity service is running

--- EntityManager/src/main/java/eu/neclab/ngsildbroker/entityhandler/services/EntityInfoDAO.java
+++ EntityManager/src/main/java/eu/neclab/ngsildbroker/entityhandler/services/EntityInfoDAO.java
@@ -1,9 +1,11 @@
 package eu.neclab.ngsildbroker.entityhandler.services;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import eu.neclab.ngsildbroker.commons.interfaces.StorageFunctionsInterface;
 import eu.neclab.ngsildbroker.commons.storage.EntityStorageFunctions;
 import eu.neclab.ngsildbroker.commons.storage.StorageDAO;
-
+@ApplicationScoped
 public class EntityInfoDAO extends StorageDAO {
 
 	@Override
