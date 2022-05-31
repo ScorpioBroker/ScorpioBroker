5d697241617bd28c7a1b67652baa5614418c2f67
Benjamin Hebgen <hebgen@neclab.eu>
2022-05-31 23:34:21 +0200

    fixed up tenant handling all working now

--- Commons/src/main/java/eu/neclab/ngsildbroker/commons/storage/ClientManager.java
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/storage/ClientManager.java
@@ -1,11 +1,15 @@
 package eu.neclab.ngsildbroker.commons.storage;
 
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.sql.SQLException;
 import java.util.HashMap;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.sql.DataSource;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,10 +24,15 @@ import io.agroal.api.configuration.supplier.AgroalDataSourceConfigurationSupplie
 import io.agroal.api.security.NamePrincipal;
 import io.agroal.api.security.SimplePassword;
 import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.tuples.Tuple2;
 import io.smallrye.mutiny.unchecked.Unchecked;
+import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.pgclient.PgPool;
+import io.vertx.mutiny.sqlclient.Pool;
 import io.vertx.mutiny.sqlclient.SqlConnection;
 import io.vertx.mutiny.sqlclient.Tuple;
+import io.vertx.pgclient.PgConnectOptions;
+import io.vertx.sqlclient.PoolOptions;
 
 @Singleton
 public class ClientManager {
@@ -36,7 +45,17 @@ public class ClientManager {
 	@Inject
 	AgroalDataSource writerDataSource;
 
+	@Inject
+	Vertx vertx;
 	// private Map<Object, DataSource> resolvedDataSources = Maps.newHashMap();
+	@ConfigProperty(name = "quarkus.datasource.reactive.url")
+	String reactiveBaseUrl;
+	@ConfigProperty(name = "quarkus.datasource.jdbc.url")
+	String jdbcBaseUrl;
+	@ConfigProperty(name = "quarkus.datasource.username")
+	String username;
+	@ConfigProperty(name = "quarkus.datasource.password")
+	String password;
 
 	protected HashMap<String, Uni<PgPool>> tenant2Client = Maps.newHashMap();
 
@@ -58,29 +77,24 @@ public class ClientManager {
 	}
 
 	private Uni<PgPool> getTenant(String tenant, boolean createDB) {
-		return determineTargetDataSource(tenant, createDB).onItem()
-				.transformToUni(Unchecked.function(finalDataSource -> {
-
-					Uni<PgPool> result = Uni.createFrom().item(pgClient.connectionProvider(t -> {
-						try {
-							return Uni.createFrom().item((SqlConnection) finalDataSource.getConnection());
-						} catch (SQLException e) {
-							return Uni.createFrom().failure(e);
-						}
-					}));
-					tenant2Client.put(tenant, result);
-					return result;
-				}));
+		return determineTargetDataSource(tenant, createDB).onItem().transformToUni(Unchecked.function(finalDataBase -> {
+			PoolOptions options = new PoolOptions();
+			PgPool pool = PgPool.pool(vertx, PgConnectOptions.fromUri(reactiveBaseUrl).setUser(username)
+					.setPassword(password).setDatabase(finalDataBase), options);
+			Uni<PgPool> result = Uni.createFrom().item(pool);
+			tenant2Client.put(tenant, result);
+			return result;
+		}));
 	}
 
-	public Uni<DataSource> determineTargetDataSource(String tenantidvalue, boolean createDB) {
+	public Uni<String> determineTargetDataSource(String tenantidvalue, boolean createDB) {
 		return createDataSourceForTenantId(tenantidvalue, createDB).onItem().transform(tenantDataSource -> {
-			flywayMigrate(tenantDataSource);
-			return tenantDataSource;
+			flywayMigrate(tenantDataSource.getItem1());
+			return tenantDataSource.getItem2();
 		});
 	}
 
-	private Uni<DataSource> createDataSourceForTenantId(String tenantidvalue, boolean createDB) {
+	private Uni<Tuple2<DataSource, String>> createDataSourceForTenantId(String tenantidvalue, boolean createDB) {
 		return findDataBaseNameByTenantId(tenantidvalue, createDB).onItem()
 				.transform(Unchecked.function(tenantDatabaseName -> {
 					// TODO this needs to be from the config not hardcoded!!!
@@ -94,7 +108,7 @@ public class ClientManager {
 											.principal(new NamePrincipal("ngb"))
 											.credential(new SimplePassword("ngb"))));
 					AgroalDataSource agroaldataSource = AgroalDataSource.from(configuration);
-					return agroaldataSource;
+					return Tuple2.of(agroaldataSource, tenantDatabaseName);
 				}));
 
 	}
@@ -104,13 +118,18 @@ public class ClientManager {
 	}
 
 	public Uni<String> findDataBaseNameByTenantId(String tenant, boolean create) {
-		String databasename = "ngb" + tenant;
+		String databasename = "ngb" + URLEncoder.encode(tenant, Charset.defaultCharset());
 		return pgClient.preparedQuery("SELECT datname FROM pg_database where datname = $1")
 				.execute(Tuple.of(databasename)).onItem().transformToUni(pgRowSet -> {
 					if (pgRowSet.size() == 0) {
+
 						if (create) {
+
 							return pgClient.preparedQuery("create database \"" + databasename + "\"").execute().onItem()
-									.transform(t -> databasename);
+									.transformToUni(t -> {
+										return storeTenantdata(tenant, databasename).onItem()
+												.transform(t2 -> databasename);
+									});
 						} else {
 							return Uni.createFrom().failure(
 									new ResponseException(ErrorType.TenantNotFound, tenant + " tenant was not found"));
@@ -123,19 +142,10 @@ public class ClientManager {
 				});
 	}
 
-	// TODO check not called by anyone... is there a good reason to record tenants
-	// like this instead of just getting the databases?
-	public void storeTenantdata(String tableName, String columnName, String tenantidvalue, String databasename)
-			throws SQLException {
-		String sql;
-		if (!tenantidvalue.equals(null)) {
-			sql = "INSERT INTO " + tableName
-					+ " (tenant_id, database_name) VALUES ($1, $2) ON CONFLICT(tenant_id) DO UPDATE SET tenant_id = EXCLUDED.tenant_id";
-			pgClient.preparedQuery(sql).executeAndForget(Tuple.of(tenantidvalue, databasename));
-		} else {
-			sql = "DELETE FROM " + tableName + " WHERE id = $1";
-			pgClient.preparedQuery(sql).executeAndForget(Tuple.of(tenantidvalue));
-		}
+	private Uni<Void> storeTenantdata(String tenantidvalue, String databasename) {
+		return pgClient.preparedQuery(
+				"INSERT INTO tenant (tenant_id, database_name) VALUES ($1, $2) ON CONFLICT(tenant_id) DO UPDATE SET tenant_id = EXCLUDED.tenant_id")
+				.execute(Tuple.of(tenantidvalue, databasename)).onItem().ignore().andContinueWithNull();
 	}
 
 	public Boolean flywayMigrate(DataSource tenantDataSource) {
