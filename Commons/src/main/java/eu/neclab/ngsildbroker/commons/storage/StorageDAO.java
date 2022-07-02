96d0ace86a53da2da43bb4b036f0f741ee84f527
Benjamin Hebgen <hebgen@neclab.eu>
2022-07-02 15:02:44 +0200

    fix for count query

--- Commons/src/main/java/eu/neclab/ngsildbroker/commons/storage/StorageDAO.java
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/storage/StorageDAO.java
@@ -244,26 +244,30 @@ public abstract class StorageDAO {
 				String sqlQuery = storageFunctions.translateNgsildQueryToSql(qp);
 				List<Map<String, Object>> result = template.queryForList(sqlQuery);
 				List<String> list = Lists.newArrayList();
-				Long count = (Long) result.get(0).get("count");
+				if (result.size() > 0) {
+					Long count = (Long) result.get(0).get("count");
+					queryResult.setCount(count);
+					long after = count - qp.getOffSet() - qp.getLimit();
+					if (after < 0) {
+						after = 0;
+					}
+					queryResult.setResultsLeftAfter(after);
+					long before = count - qp.getOffSet();
+					if (before < 0) {
+						before = 0;
+					}
+					queryResult.setResultsLeftBefore(before);
+				}
 				for (Map<String, Object> entry : result) {
 					list.add((String) entry.get("data").toString());
 				}
 
 				queryResult.setDataString(list);
 				queryResult.setActualDataString(list);
-				queryResult.setCount(count);
+
 				queryResult.setOffset(qp.getOffSet());
 				queryResult.setLimit(qp.getLimit());
-				long after = count - qp.getOffSet() - qp.getLimit();
-				if (after < 0) {
-					after = 0;
-				}
-				queryResult.setResultsLeftAfter(after);
-				long before = count - qp.getOffSet();
-				if (before < 0) {
-					before = 0;
-				}
-				queryResult.setResultsLeftBefore(before);
+
 				return queryResult;
 			} else {
 				String sqlQuery = storageFunctions.translateNgsildQueryToSql(qp);
