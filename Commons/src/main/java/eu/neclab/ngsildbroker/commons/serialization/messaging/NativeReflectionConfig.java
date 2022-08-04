a9ba1c09df99398c970a24aa9236f50ba962709b
Benjamin Hebgen <hebgen@neclab.eu>
2022-08-04 15:17:10 +0200

    changing register for reflection

---
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/serialization/messaging/NativeReflectionConfig.java
@@ -0,0 +1,13 @@
+package eu.neclab.ngsildbroker.commons.serialization.messaging;
+
+import com.google.common.collect.ArrayListMultimap;
+
+import eu.neclab.ngsildbroker.commons.datatypes.Subscription;
+import eu.neclab.ngsildbroker.commons.datatypes.requests.SubscriptionRequest;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection(targets = { ArrayListMultimap.class, Subscription.class,
+		SubscriptionRequest.class }, serialization = true)
+public class NativeReflectionConfig {
+
+}
