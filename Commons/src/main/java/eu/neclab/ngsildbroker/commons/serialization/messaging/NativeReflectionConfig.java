b4930379106027d6f859adbae5184edd37f3ea5d
Benjamin Hebgen <hebgen@neclab.eu>
2022-08-04 16:30:37 +0200

    testing reflection stuff

--- Commons/src/main/java/eu/neclab/ngsildbroker/commons/serialization/messaging/NativeReflectionConfig.java
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/serialization/messaging/NativeReflectionConfig.java
@@ -6,8 +6,7 @@ import eu.neclab.ngsildbroker.commons.datatypes.Subscription;
 import eu.neclab.ngsildbroker.commons.datatypes.requests.SubscriptionRequest;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
-@RegisterForReflection(targets = { ArrayListMultimap.class, Subscription.class,
-		SubscriptionRequest.class }, serialization = true)
+@RegisterForReflection(targets = { ArrayListMultimap.class, SubscriptionRequest.class, Subscription.class })
 public class NativeReflectionConfig {
 
 }
