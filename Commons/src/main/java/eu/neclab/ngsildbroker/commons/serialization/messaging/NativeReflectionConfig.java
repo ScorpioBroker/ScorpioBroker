b94fb2555bc56fb00956998f2932ac54c0ac5a36
Benjamin Hebgen <hebgen@neclab.eu>
2022-08-08 10:33:34 +0200

    some inital tests with consul registry and added some native instructs

--- Commons/src/main/java/eu/neclab/ngsildbroker/commons/serialization/messaging/NativeReflectionConfig.java
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/serialization/messaging/NativeReflectionConfig.java
@@ -6,7 +6,8 @@ import eu.neclab.ngsildbroker.commons.datatypes.Subscription;
 import eu.neclab.ngsildbroker.commons.datatypes.requests.SubscriptionRequest;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
-@RegisterForReflection(targets = { ArrayListMultimap.class, SubscriptionRequest.class, Subscription.class })
+@RegisterForReflection(targets = { ArrayListMultimap.class, SubscriptionRequest.class,
+		Subscription.class }, serialization = true)
 public class NativeReflectionConfig {
 
 }
