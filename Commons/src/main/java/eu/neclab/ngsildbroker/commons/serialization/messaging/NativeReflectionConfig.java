a2cb1d9cb4ff8d0ff5658ca5f39c2284af8e5b4a
Benjamin Hebgen <hebgen@neclab.eu>
2022-08-09 15:05:56 +0200

    added georelation to reflection registry

--- Commons/src/main/java/eu/neclab/ngsildbroker/commons/serialization/messaging/NativeReflectionConfig.java
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/serialization/messaging/NativeReflectionConfig.java
@@ -5,6 +5,7 @@ import com.google.common.collect.ArrayListMultimap;
 import eu.neclab.ngsildbroker.commons.datatypes.AliveAnnouncement;
 import eu.neclab.ngsildbroker.commons.datatypes.EndPoint;
 import eu.neclab.ngsildbroker.commons.datatypes.EntityInfo;
+import eu.neclab.ngsildbroker.commons.datatypes.GeoRelation;
 import eu.neclab.ngsildbroker.commons.datatypes.LDGeoQuery;
 import eu.neclab.ngsildbroker.commons.datatypes.LDTemporalQuery;
 import eu.neclab.ngsildbroker.commons.datatypes.NotificationParam;
@@ -15,7 +16,7 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection(targets = { ArrayListMultimap.class, SubscriptionRequest.class, Subscription.class,
 		NotificationParam.class, EndPoint.class, EntityInfo.class, LDTemporalQuery.class, LDGeoQuery.class,
-		SyncMessage.class, AliveAnnouncement.class }, serialization = true)
+		SyncMessage.class, AliveAnnouncement.class, GeoRelation.class }, serialization = true)
 public class NativeReflectionConfig {
 
 }
