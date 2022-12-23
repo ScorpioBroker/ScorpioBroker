285a39798bf5ddd5fbd9bde81a5eeb757787e2e0
ScorpioBroker <55130839+ScorpioBroker@users.noreply.github.com>
2022-12-23 15:13:21 +0100

    wip

--- Commons/src/main/java/eu/neclab/ngsildbroker/commons/storage/EntityStorageFunctions.java
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/storage/EntityStorageFunctions.java
@@ -1,20 +1,17 @@
 package eu.neclab.ngsildbroker.commons.storage;
 
-import static eu.neclab.ngsildbroker.commons.interfaces.StorageFunctionsInterface.getSQLList;
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
+
+import com.google.common.collect.Lists;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.google.common.collect.Lists;
-import io.smallrye.mutiny.Uni;
-import io.smallrye.mutiny.tuples.Tuple3;
-import io.vertx.mutiny.sqlclient.Row;
-import io.vertx.mutiny.sqlclient.RowSet;
-import io.vertx.mutiny.sqlclient.SqlConnection;
-import io.vertx.mutiny.sqlclient.Tuple;
+
 import eu.neclab.ngsildbroker.commons.constants.DBConstants;
 import eu.neclab.ngsildbroker.commons.constants.NGSIConstants;
 import eu.neclab.ngsildbroker.commons.datatypes.GeoqueryRel;
@@ -22,6 +19,13 @@ import eu.neclab.ngsildbroker.commons.datatypes.QueryParams;
 import eu.neclab.ngsildbroker.commons.enums.ErrorType;
 import eu.neclab.ngsildbroker.commons.exceptions.ResponseException;
 import eu.neclab.ngsildbroker.commons.interfaces.StorageFunctionsInterface;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.tuples.Tuple3;
+import io.vertx.mutiny.sqlclient.Row;
+import io.vertx.mutiny.sqlclient.RowSet;
+import io.vertx.mutiny.sqlclient.SqlConnection;
+import io.vertx.mutiny.sqlclient.Tuple;
+import static eu.neclab.ngsildbroker.commons.interfaces.StorageFunctionsInterface.getSQLList;
 
 public class EntityStorageFunctions implements StorageFunctionsInterface {
 
@@ -95,7 +99,6 @@ public class EntityStorageFunctions implements StorageFunctionsInterface {
 	// TODO: property of property
 	// [SPEC] spec is not clear on how to define a "property of property" in
 	// the geoproperty field. (probably using dots)
-	@Override
 	public Tuple3<String, ArrayList<Object>, Integer> translateNgsildGeoqueryToPostgisQuery(GeoqueryRel georel,
 			String geometry, String coordinates, String geoproperty, int currentCount) throws ResponseException {
 		StringBuilder sqlWhere = new StringBuilder(50);
@@ -113,46 +116,29 @@ public class EntityStorageFunctions implements StorageFunctionsInterface {
 		String sqlPostgisFunction = DBConstants.NGSILD_TO_POSTGIS_GEO_OPERATORS_MAPPING.get(georelOp);
 
 		switch (georelOp) {
-			case NGSIConstants.GEO_REL_NEAR:
-				if (georel.getDistanceType() != null && georel.getDistanceValue() != null) {
-					if (georel.getDistanceType().equals(NGSIConstants.GEO_REL_MIN_DISTANCE))
-						sqlWhere.append("NOT ");
-					sqlWhere.append(sqlPostgisFunction + "( " + dbColumn + "::geography, " + referenceValue
-							+ "::geography, " + georel.getDistanceValue() + ") ");
-				} else {
-					throw new ResponseException(ErrorType.BadRequestData,
-							"GeoQuery: Type and distance are required for near relation");
-				}
-				break;
-			case NGSIConstants.GEO_REL_WITHIN:
-			case NGSIConstants.GEO_REL_CONTAINS:
-			case NGSIConstants.GEO_REL_OVERLAPS:
-			case NGSIConstants.GEO_REL_INTERSECTS:
-			case NGSIConstants.GEO_REL_EQUALS:
-			case NGSIConstants.GEO_REL_DISJOINT:
-				sqlWhere.append(sqlPostgisFunction + "( " + dbColumn + ", " + referenceValue + ") ");
-				break;
-			default:
-				throw new ResponseException(ErrorType.BadRequestData, "Invalid georel operator: " + georelOp);
+		case NGSIConstants.GEO_REL_NEAR:
+			if (georel.getDistanceType() != null && georel.getDistanceValue() != null) {
+				if (georel.getDistanceType().equals(NGSIConstants.GEO_REL_MIN_DISTANCE))
+					sqlWhere.append("NOT ");
+				sqlWhere.append(sqlPostgisFunction + "( " + dbColumn + "::geography, " + referenceValue
+						+ "::geography, " + georel.getDistanceValue() + ") ");
+			} else {
+				throw new ResponseException(ErrorType.BadRequestData,
+						"GeoQuery: Type and distance are required for near relation");
+			}
+			break;
+		case NGSIConstants.GEO_REL_WITHIN:
+		case NGSIConstants.GEO_REL_CONTAINS:
+		case NGSIConstants.GEO_REL_OVERLAPS:
+		case NGSIConstants.GEO_REL_INTERSECTS:
+		case NGSIConstants.GEO_REL_EQUALS:
+		case NGSIConstants.GEO_REL_DISJOINT:
+			sqlWhere.append(sqlPostgisFunction + "( " + dbColumn + ", " + referenceValue + ") ");
+			break;
+		default:
+			throw new ResponseException(ErrorType.BadRequestData, "Invalid georel operator: " + georelOp);
 		}
 		return Tuple3.of(sqlWhere.toString(), Lists.newArrayList(), currentCount);
-	}
-
-	@Override
-	public Uni<RowSet<Row>> translateNgsildQueryToCountResult(QueryParams qp, SqlConnection conn) {
-		Tuple3<String, ArrayList<Object>, Integer> fullSqlWhereProperty;
-		try {
-			fullSqlWhereProperty = commonTranslateSql(qp, 1);
-		} catch (ResponseException e) {
-			return Uni.createFrom().failure(e);
-		}
-		String tableName = DBConstants.DBTABLE_ENTITY;
-		String sqlQuery = "SELECT Count(*) FROM " + tableName + " ";
-		if (fullSqlWhereProperty.getItem1().length() > 0) {
-			sqlQuery += "WHERE " + fullSqlWhereProperty.toString() + " ";
-		}
-		logger.debug("SQL Query for count: " + sqlQuery);
-		return conn.preparedQuery(sqlQuery).execute(Tuple.from(fullSqlWhereProperty.getItem2()));
 	}
 
 	private Tuple3<String, ArrayList<Object>, Integer> commonTranslateSql(QueryParams qp, int currentCount)
@@ -171,45 +157,43 @@ public class EntityStorageFunctions implements StorageFunctionsInterface {
 				entitiesAdded = true;
 				for (Entry<String, String> entry : entityInfo.entrySet()) {
 					switch (entry.getKey()) {
-						case NGSIConstants.JSON_LD_ID:
-							dbColumn = NGSIConstants.QUERY_PARAMETER_ID;
-							if (entry.getValue().indexOf(",") == -1) {
+					case NGSIConstants.JSON_LD_ID:
+						dbColumn = NGSIConstants.QUERY_PARAMETER_ID;
+						if (entry.getValue().indexOf(",") == -1) {
 
-								sqlWhereProperty = dbColumn + " = $" + currentCount;
-								currentCount++;
-								replacements.add(entry.getValue());
-							} else {
-								Tuple3<String, ArrayList<Object>, Integer> tmp = getSQLList(entry.getValue(),
-										currentCount);
-								currentCount = tmp.getItem3();
-								replacements.addAll(tmp.getItem2());
-								sqlWhereProperty = dbColumn + " IN (" + tmp.getItem1() + ")";
-							}
-							break;
-						case NGSIConstants.JSON_LD_TYPE:
-							dbColumn = NGSIConstants.QUERY_PARAMETER_TYPE;
-							if (entry.getValue().indexOf(",") == -1) {
-								sqlWhereProperty = dbColumn + " = $" + currentCount;
-								currentCount++;
-								replacements.add(entry.getValue());
-							} else {
-								Tuple3<String, ArrayList<Object>, Integer> tmp = getSQLList(entry.getValue(),
-										currentCount);
-								currentCount = tmp.getItem3();
-								replacements.addAll(tmp.getItem2());
-								sqlWhereProperty = dbColumn + " IN (" + tmp.getItem1() + ")";
-							}
-
-							break;
-						case NGSIConstants.NGSI_LD_ID_PATTERN:
-							dbColumn = DBConstants.DBCOLUMN_ID;
-							sqlWhereProperty = dbColumn + " ~ $" + currentCount;
+							sqlWhereProperty = dbColumn + " = $" + currentCount;
 							currentCount++;
 							replacements.add(entry.getValue());
-							break;
+						} else {
+							Tuple3<String, ArrayList<Object>, Integer> tmp = getSQLList(entry.getValue(), currentCount);
+							currentCount = tmp.getItem3();
+							replacements.addAll(tmp.getItem2());
+							sqlWhereProperty = dbColumn + " IN (" + tmp.getItem1() + ")";
+						}
+						break;
+					case NGSIConstants.JSON_LD_TYPE:
+						dbColumn = NGSIConstants.QUERY_PARAMETER_TYPE;
+						if (entry.getValue().indexOf(",") == -1) {
+							sqlWhereProperty = dbColumn + " = $" + currentCount;
+							currentCount++;
+							replacements.add(entry.getValue());
+						} else {
+							Tuple3<String, ArrayList<Object>, Integer> tmp = getSQLList(entry.getValue(), currentCount);
+							currentCount = tmp.getItem3();
+							replacements.addAll(tmp.getItem2());
+							sqlWhereProperty = dbColumn + " IN (" + tmp.getItem1() + ")";
+						}
 
-						default:
-							break;
+						break;
+					case NGSIConstants.NGSI_LD_ID_PATTERN:
+						dbColumn = DBConstants.DBCOLUMN_ID;
+						sqlWhereProperty = dbColumn + " ~ $" + currentCount;
+						currentCount++;
+						replacements.add(entry.getValue());
+						break;
+
+					default:
+						break;
 					}
 					fullSqlWhereProperty.append(sqlWhereProperty);
 					fullSqlWhereProperty.append(" AND ");
@@ -267,9 +251,27 @@ public class EntityStorageFunctions implements StorageFunctionsInterface {
 		if (qp.getQ() != null) {
 			// TODO SQL escape q;
 			sqlWhereProperty = qp.getQ();
+			fullSqlWhereProperty.append(" AND ");
 			fullSqlWhereProperty.append(sqlWhereProperty);
 		}
 		return Tuple3.of(fullSqlWhereProperty.toString(), replacements, currentCount);
+	}
+
+	@Override
+	public Uni<RowSet<Row>> translateNgsildQueryToCountResult(QueryParams qp, SqlConnection conn) {
+		Tuple3<String, ArrayList<Object>, Integer> fullSqlWhereProperty;
+		try {
+			fullSqlWhereProperty = commonTranslateSql(qp, 1);
+		} catch (ResponseException e) {
+			return Uni.createFrom().failure(e);
+		}
+		String tableName = DBConstants.DBTABLE_ENTITY;
+		String sqlQuery = "SELECT Count(*) FROM " + tableName + " ";
+		if (fullSqlWhereProperty.getItem1().length() > 0) {
+			sqlQuery += "WHERE " + fullSqlWhereProperty.toString() + " ";
+		}
+		logger.debug("SQL Query for count: " + sqlQuery);
+		return conn.preparedQuery(sqlQuery).execute(Tuple.from(fullSqlWhereProperty.getItem2()));
 	}
 
 	@Override
