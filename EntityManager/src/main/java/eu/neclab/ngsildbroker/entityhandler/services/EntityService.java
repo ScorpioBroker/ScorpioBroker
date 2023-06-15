4aed6154a60a23a267d94db9920e366933e6cd1b
ScorpioBroker <55130839+ScorpioBroker@users.noreply.github.com>
2023-06-15 21:44:22 +0200

    testing

--- EntityManager/src/main/java/eu/neclab/ngsildbroker/entityhandler/services/EntityService.java
+++ EntityManager/src/main/java/eu/neclab/ngsildbroker/entityhandler/services/EntityService.java
@@ -669,7 +669,7 @@ public class EntityService {
 		return entityDAO.createEntity(request).onItem().transform(v -> {
 			NGSILDOperationResult localResult = new NGSILDOperationResult(AppConstants.CREATE_REQUEST, request.getId());
 			localResult.addSuccess(new CRUDSuccess(null, null, null, request.getPayload(), context));
-			entityEmitter.sendAndForget(request);
+			//entityEmitter.sendAndForget(request);
 			return localResult;
 		});
 
