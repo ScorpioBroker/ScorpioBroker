3954457b38f2e9599a7e2121ef025ff16cf90b13
Brijesh <brijesh@nectechnologies.in>
2023-06-21 12:12:07 +0530

    syntax error fixed

--- QueryManager/src/main/java/eu/neclab/ngsildbroker/queryhandler/services/QueryService.java
+++ QueryManager/src/main/java/eu/neclab/ngsildbroker/queryhandler/services/QueryService.java
@@ -182,8 +182,8 @@ public class QueryService {
 		if (unis.isEmpty()) {
 			QueryResult q = new QueryResult();
 			if(count){
-				q.setCount(resultCount)
-			};
+				q.setCount(resultCount);
+			}
 			q.setData(new ArrayList<Map<String, Object>>());
 			return Uni.createFrom().item(q);
 		}
