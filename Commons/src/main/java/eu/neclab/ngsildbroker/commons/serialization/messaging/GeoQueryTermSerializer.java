f30c2855f2df5b8030fd2bf313da42429f628de3
ScorpioBroker <55130839+ScorpioBroker@users.noreply.github.com>
2023-07-18 21:24:12 +0200

    trying it with a serializer let's see

---
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/serialization/messaging/GeoQueryTermSerializer.java
@@ -0,0 +1,8 @@
+package eu.neclab.ngsildbroker.commons.serialization.messaging;
+
+import eu.neclab.ngsildbroker.commons.datatypes.terms.GeoQueryTerm;
+import io.quarkus.kafka.client.serialization.ObjectMapperSerializer;
+
+public class GeoQueryTermSerializer extends ObjectMapperSerializer<GeoQueryTerm> {
+
+}
