1e4710657853443763122c3526dc038a2be842c4
ScorpioBroker <55130839+ScorpioBroker@users.noreply.github.com>
2023-07-18 21:28:10 +0200

    no extra serializer

--- Commons/src/main/java/eu/neclab/ngsildbroker/commons/serialization/messaging/GeoQueryTermSerializer.java
+++
@@ -1,8 +0,0 @@
-package eu.neclab.ngsildbroker.commons.serialization.messaging;
-
-import eu.neclab.ngsildbroker.commons.datatypes.terms.GeoQueryTerm;
-import io.quarkus.kafka.client.serialization.ObjectMapperSerializer;
-
-public class GeoQueryTermSerializer extends ObjectMapperSerializer<GeoQueryTerm> {
-
-}
