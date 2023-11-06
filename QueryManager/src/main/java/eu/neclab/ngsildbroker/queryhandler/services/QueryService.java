c46f0af8e97d774eb8612024b0cf34ca43a16e32
brijesh <brijesh@india.nec.com>
2023-11-06 15:12:56 +0530

    getting entity with no attrs fix

--- QueryManager/src/main/java/eu/neclab/ngsildbroker/queryhandler/services/QueryService.java
+++ QueryManager/src/main/java/eu/neclab/ngsildbroker/queryhandler/services/QueryService.java
@@ -251,16 +251,16 @@ public class QueryService {
 					.entrySet()) {
 				String entityId = entry.getKey();
 				Map<String, Map<String, Map<String, Object>>> attribMap = entry.getValue();
-				if (attribMap.isEmpty()) {
-					logger.warn("failed to merge entity id: " + entityId);
-					logger.warn("" + entityId2Types.containsKey(entityId));
-					if (attribMap != null) {
-						logger.warn(attribMap.toString());
-					} else {
-						logger.warn("null attribmap");
-					}
-					continue;
-				}
+//				if (attribMap.isEmpty()) {
+//					logger.warn("failed to merge entity id: " + entityId);
+//					logger.warn("" + entityId2Types.containsKey(entityId));
+//					if (attribMap != null) {
+//						logger.warn(attribMap.toString());
+//					} else {
+//						logger.warn("null attribmap");
+//					}
+//					continue;
+//				}
 
 				Map<String, Object> entity = new HashMap<>(attribMap.size() + 5);
 				entity.put(NGSIConstants.JSON_LD_ID, entityId);
