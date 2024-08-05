23bd83a6b968cc6e6d9f0254878799fcc042417c
ScorpioBroker <55130839+ScorpioBroker@users.noreply.github.com>
2024-08-05 14:58:52 +0200

    wip

--- AllInOneRunner/src/main/resources/db/migration/V20240530.1__entitymapupdate.sql
+++ AllInOneRunner/src/main/resources/db/migration/V20240530.1__entitymapupdate.sql
@@ -4,6 +4,58 @@ DROP TABLE IF EXISTS public.entitymap_management;
 DROP FUNCTION IF EXISTS ngsild_appendbatch(jsonb);
 DROP FUNCTION IF EXISTS ngsild_upsertbatch(jsonb);
 
+CREATE OR REPLACE FUNCTION public.ngsild_upsertbatch(IN entities jsonb,IN do_replace boolean)
+    RETURNS jsonb
+    LANGUAGE 'plpgsql'
+    VOLATILE
+    PARALLEL UNSAFE
+    COST 100
+    
+AS $BODY$
+declare
+	resultObj jsonb;
+	resultEntry jsonb;
+	newentity jsonb;
+	prev_entity jsonb;
+	updated_entity jsonb;
+	updated boolean;
+BEGIN
+	resultObj := '{"success": [], "failure": []}'::jsonb;
+	FOR newentity IN SELECT jsonb_array_elements FROM jsonb_array_elements(ENTITIES) LOOP
+		prev_entity := NULL;
+		updated := FALSE;
+		BEGIN
+			IF newentity ? '@type' THEN
+				INSERT INTO ENTITY(ID,E_TYPES, ENTITY) VALUES (newentity->>'@id',  ARRAY(SELECT jsonb_array_elements_text(newentity->'@type')), newentity) RETURNING ENTITY.ENTITY INTO updated_entity;
+			ELSEIF DO_REPLACE THEN
+				SELECT ENTITY FROM ENTITY WHERE ID=newentity->>'@id' INTO prev_entity;
+				UPDATE ENTITY SET ENTITY = newentity ||jsonb_build_object('@type',(ENTITY->'@type')) WHERE id = newentity->>'@id' RETURNING ENTITY.ENTITY INTO updated_entity;
+				updated := TRUE;
+			ELSE
+				SELECT ENTITY FROM ENTITY WHERE ID=newentity->>'@id' INTO prev_entity;
+				UPDATE ENTITY SET ENTITY = ENTITY.ENTITY || newentity WHERE id = newentity->>'@id' RETURNING ENTITY.ENTITY INTO updated_entity;
+				updated := TRUE;
+			END IF;
+			resultObj = jsonb_set(resultObj, '{success}', resultObj -> 'success' || jsonb_build_object('id', (newentity->>'@id'), 'updated', updated, 'old', prev_entity, 'new', updated_entity));
+		EXCEPTION WHEN unique_violation THEN
+			SELECT ENTITY FROM ENTITY WHERE ID=newentity->>'@id' INTO prev_entity;
+			IF DO_REPLACE THEN
+				UPDATE ENTITY SET E_TYPES = ARRAY(SELECT DISTINCT UNNEST(ARRAY(SELECT jsonb_array_elements_text(newentity->'@type')))), ENTITY = newentity WHERE ID=newentity->>'@id' RETURNING ENTITY.entity INTO updated_entity;
+			ELSE
+				UPDATE ENTITY SET E_TYPES = ARRAY(SELECT DISTINCT UNNEST(e_types || ARRAY(SELECT jsonb_array_elements_text(newentity->'@type')))), ENTITY = ENTITY.entity || newentity || jsonb_set(newentity, '{@type}', array_to_json(ARRAY(SELECT DISTINCT UNNEST(e_types || ARRAY(SELECT jsonb_array_elements_text(newentity->'@type'))))) ::jsonb)
+				WHERE ID=newentity->>'@id' RETURNING ENTITY.entity INTO updated_entity;
+			END IF;
+
+			updated := TRUE;
+			resultObj = jsonb_set(resultObj, '{success}', resultObj -> 'success' || jsonb_build_object('id', (newentity->>'@id'), 'updated', updated, 'old', prev_entity, 'new', updated_entity));
+		WHEN OTHERS THEN
+			resultObj = jsonb_set(resultObj, '{failure}', resultObj -> 'failure' || jsonb_build_object(newentity->>'@id', SQLSTATE));
+		END;
+	END LOOP;
+	RETURN resultObj;
+END;
+$BODY$;
+
 CREATE TABLE public.entitymap
 (
 		id text,
