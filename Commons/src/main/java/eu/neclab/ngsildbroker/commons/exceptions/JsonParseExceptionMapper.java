3bdb7b681be0a393ccceb76839ce9d2175557da6
ScorpioBroker <55130839+ScorpioBroker@users.noreply.github.com>
2025-03-19 15:53:48 +0100

    added better error messages

---
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/exceptions/JsonParseExceptionMapper.java
@@ -0,0 +1,21 @@
+package eu.neclab.ngsildbroker.commons.exceptions;
+
+import com.fasterxml.jackson.core.JsonParseException;
+
+import eu.neclab.ngsildbroker.commons.enums.ErrorType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class JsonParseExceptionMapper implements ExceptionMapper<JsonParseException> {
+
+	@Override
+	public Response toResponse(JsonParseException exception) {
+		return Response.status(Response.Status.BAD_REQUEST)
+				.entity(new ResponseException(ErrorType.InvalidRequest,
+						"There is an error in the provided json document").getJson())
+				.header("Content-Type", "application/json").build();
+	}
+
+}
