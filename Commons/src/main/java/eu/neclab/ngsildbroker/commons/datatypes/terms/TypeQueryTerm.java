993705b750ed569707285a4c002202f7ef7585bf
ScorpioBroker <55130839+ScorpioBroker@users.noreply.github.com>
2025-09-10 16:46:11 +0200

    wip

--- Commons/src/main/java/eu/neclab/ngsildbroker/commons/datatypes/terms/TypeQueryTerm.java
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/datatypes/terms/TypeQueryTerm.java
@@ -218,7 +218,7 @@ public class TypeQueryTerm implements Serializable {
 			while (current.firstChild != null) {
 				current = current.firstChild;
 			}
-			current.next.toSql(result, tuple, dollar);
+			dollar = current.toSql(result, tuple, dollar);
 		} else {
 			result.append("(e_types ");
 			if (next != null && nextAnd) {
